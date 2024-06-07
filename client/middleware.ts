961085999b4831812878cb336ae3089b62f86cc3
Muhammed Bilal <126692548+bilalxmhd@users.noreply.github.com>
2024-06-07 20:17:39 +0530

    Update middleware.ts

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,8 +1,26 @@
+// import { authMiddleware } from "@clerk/nextjs/server";
+
+// export default authMiddleware({
+//   publicRoutes: ["/", "/courses", "/course-preview/(.*)"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+import { NextRequest, NextResponse } from "next/server";
 import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/courses", "/course-preview/(.*)"],
-});
+// A simple response function for edge environment
+const edgeHandler = (req: NextRequest) => {
+  return NextResponse.next(); // Allow all requests in edge environment
+};
+
+// Main middleware function
+export default async function middleware(req: NextRequest, ev: any) {
+  return authMiddleware({
+    publicRoutes: ["/", "/courses", "/course-preview/(.*)"],
+  })(req, ev);
+}
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
